aecf00b9e6424f891c75adef935432539f13e9a6
ankit mishra <mishraankit987@gmail.com>
2024-02-23 17:40:52 +0530

    updated password checking in server controller

--- server/Controllers/user.controller.js
+++ server/Controllers/user.controller.js
@@ -87,7 +87,7 @@ const login=async (req,res,next)=>{
     const user=await User.findOne({
       email
     }).select('+password')
-    if(!user||!user.comparePassword(password)){
+    if(!user||! await user.comparePassword(password)){
       return next(new AppError('email or password is incorrect',400))
     }
     user.password=undefined;
